3260d57c747f6d6f7773497ba9e79b7058aefc3d
Tímea Tamasi <103537849+DoomP1nky@users.noreply.github.com>
2023-01-23 13:53:48 +0100

    add error msg

--- frontend/script.js
+++ frontend/script.js
@@ -1,19 +1,25 @@
 const loadEvent = (_) => {
   function getWeather(city) {
-    document.getElementById("loading-spinner").style.display = "block";
     // Fetch weather data for the selected city
     const API_KEY = 'ba8cd0c0041848dd91a191032232101';
-    const API_URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`;
-  
+    const API_URL = `https://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${city}`;
+
     fetch(API_URL)
-      .then(response => response.json())
-      .then(data => {
-        let temperature = data.current.temp_c;
-        let skyConditions = data.current.condition.text;
-        let humidity = data.current.humidity;
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const temperature = data.current.temp_c;
+        const skyConditions = data.current.condition.text;
+        const humidity = data.current.humidity;
+      })
+      .catch((error) => {
+        //handle error if data is not available in the API
+        console.log(error);
+        alert('Sorry, the selected data is not available');
       });
   }
-  getWeather();
+  const location = prompt();
+  getWeather(location);
 };
 // you can run your code in different ways but this is the safest. This way you can make sure that all the content (including css, fonts) is loaded.
 window.addEventListener('load', loadEvent);
