92c5e69675acf60a9d7aee58cded3aa99f47acbb
Tímea Tamasi <103537849+DoomP1nky@users.noreply.github.com>
2023-01-23 13:17:46 +0100

    clone server from Nasa project, delete content

---
+++ frontend/script.js
@@ -0,0 +1,5 @@
+const loadEvent = (_) => {
+  
+};
+// you can run your code in different ways but this is the safest. This way you can make sure that all the content (including css, fonts) is loaded.
+window.addEventListener('load', loadEvent);
