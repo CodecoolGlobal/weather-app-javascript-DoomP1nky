0f33c40e58c9effe7235b5c5c73b081a1fd9c18b
Tímea Tamasi <103537849+DoomP1nky@users.noreply.github.com>
2023-01-23 23:20:51 +0100

    fixed the citySelector and add click event to chose the target city

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,23 @@
 const loadEvent = (_) => {
   const root = document.getElementById('root');
 
+  // Create input field
   const citySelector = document.createElement('input');
   citySelector.type = 'search';
   citySelector.id = 'search';
   citySelector.placeholder = 'Select a city...';
+  citySelector.autocomplete = 'on';
   root.appendChild(citySelector);
+
+  // Create list element for search matches
+  const matchList = document.createElement('ul');
+  matchList.id = 'match-list';
+  root.appendChild(matchList);
+
+  // Create weather card container
+  const weatherCard = document.createElement('div');
+  weatherCard.id = 'weather-card';
+  root.appendChild(weatherCard);
 
   citySelector.addEventListener('input', () => {
     if (citySelector.value.length >= 3){
@@ -13,24 +25,29 @@ const loadEvent = (_) => {
       // Fetch weather data for the selected city
       const API_KEY = 'ba8cd0c0041848dd91a191032232101';
       const API_URL = `https://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${citySelector.value}`;
-      
-      fetch(API_URL)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        
-        /*  const temperature = data.current.temp_c;
-        const skyConditions = data.current.condition.text;
-        const humidity = data.current.humidity; */
-      })
-      .catch((error) => {
-        //handle error if data is not available in the API
-        console.log(error);
-        alert('Sorry, the selected data is not available');
-      });
-    }
-    });
-};
 
-// you can run your code in different ways but this is the safest. This way you can make sure that all the content (including css, fonts) is loaded.
+      fetch(API_URL)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          matchList.innerHTML = '';
+          // Iterate through search matchess
+          data.forEach((match) => {
+          // Create list item for match
+            const li = document.createElement('li');
+            li.innerText = match.name;
+            matchList.appendChild(li);
+
+            //add eventlistener to list item
+            li.addEventListener('click', (e) => {
+              citySelector.value = e.target.innerText;
+              matchList.innerHTML = '';
+            });
+          });
+        });
+    }
+  });
+
+};
+//you can run your code in different ways but this is the safest. This way you can make sure that all the content (including css, fonts) is loaded.
 window.addEventListener('load', loadEvent);
